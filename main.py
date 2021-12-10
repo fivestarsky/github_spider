f7d5efa9927772a0ddcfb30c975f84f0d673a13f
fivestarsky <fivestarsky@gmail.com>
2021-12-10 15:27:26 +0800

    re opensearch

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import json
-from dynaconf import settings
-import requests
-import util.opensearch_api as opensearch_api
-import util.opensearch_client as opensearch_client
-import spider.github_commits as github_commits
+import util.opensearch.opensearch_client as opensearch_client
 
 if __name__ == '__main__':
     opensearch_client.sync_github_commits("apache", "spark")
--- util/opensearch/opensearch.py
+++ util/opensearch/opensearch.py
@@ -2,6 +2,19 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 from dynaconf import settings
+from opensearchpy import OpenSearch
+from opensearchpy import helpers as OpenSearchHelpers
+import spider.github_commits as github_commits
+
+client = OpenSearch(
+    hosts=[{'host': settings.OPENSEARCH_CONFIG.HOST, 'port': settings.OPENSEARCH_CONFIG.PORT}],
+    http_compress=True,
+    http_auth=(settings.OPENSEARCH_CONFIG.HTTP_BASIC_AUTH_USER, settings.OPENSEARCH_CONFIG.HTTP_BASIC_AUTH_PASS),
+    use_ssl=True,
+    verify_certs=False,
+    ssl_assert_hostname=False,
+    ssl_show_warn=False
+)
 
 
 def search_sql(sql_text):
@@ -50,3 +63,23 @@ def delete_index(index_name):
                         headers={'content-type': 'application/json'},
                         )
     return json.dumps(r.json(), sort_keys=True, indent=2)
+
+
+def sync_github_commits(owner, repo, since=None, until=None):
+    success, failed = 0, 0
+    all_github_commits = github_commits.get_all_github_commits(owner, repo, since, until)
+    if (all_github_commits is not None) and (len(all_github_commits > 0)):
+        bulk_all_github_commits = []
+        template = {"_index": "github_commits",
+                    "_source": None}
+        for now_commit in all_github_commits:
+            commit_item = template.copy()
+            commit_item["_source"] = now_commit
+            bulk_all_github_commits.append(commit_item)
+        success, failed = OpenSearchHelpers.bulk(client=client, actions=bulk_all_github_commits)
+
+    return success, failed
+
+
+def get_all_github_issues(owner, repo, since=None, until=None):
+    print("")
